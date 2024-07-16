44f1581436f38b1929b736c91c446026af3e0ef4
Carter Hinsley <carterhinsley@outlook.com>
2024-07-16 01:54:25 -0400

    Remove react and vite stuff

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Override MUI theme.
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 // Import Roboto font for MUI.
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,7 +14,9 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
 )
