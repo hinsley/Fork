1e3af89fbc6e5d237efc2b4b9c2e303716883e28
Carter Hinsley <carterhinsley@outlook.com>
2024-07-16 12:55:16 -0400

    Add logo to header

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import StateSpace from './components/StateSpace.tsx'
 function App() {
   return (
     <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-      <h1>Fork</h1>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 2, mt: 2 }}>
+        <img src="/favicon.svg" alt="Fork logo" style={{ height: '2em', marginRight: '0.5em' }} />
+        <h1 style={{ margin: 0 }}>Fork</h1>
+      </Box>
       <Box sx={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }}>
         <Box sx={{ flex: 2, overflow: 'auto', pr: 2 }}>
           <StateSpace />
--- src/components/StateSpace.tsx
+++ src/components/StateSpace.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Box } from '@mui/material'
 import { ContainedMathbox, Axis, Grid, Cartesian } from 'mathbox-react'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
@@ -10,17 +11,20 @@ const mathboxOptions = {
 }
 
 const StateSpace: React.FC = () => (
-	<ContainedMathbox
-		options={mathboxOptions}
-		containerStyle={{ height: "100%", width: "100%" }}
-	>
-		<Cartesian>
-			<Axis axis="x" color="orange" />
-			<Axis axis="y" color="blue" />
-			<Axis axis="z" color="green" />
-			<Grid axes="xz" />
-		</Cartesian>
-	</ContainedMathbox>
+	<Box sx={{ height: '100%', width: '100%', overflow: 'hidden' }}>
+		<h3>State Space</h3>
+		<ContainedMathbox
+			options={mathboxOptions}
+			containerStyle={{ height: "100%", width: "100%" }}
+		>
+			<Cartesian>
+				<Axis axis="x" color="orange" />
+				<Axis axis="y" color="blue" />
+				<Axis axis="z" color="green" />
+				<Grid axes="xz" />
+			</Cartesian>
+		</ContainedMathbox>
+	</Box>
 )
 
 export default StateSpace
